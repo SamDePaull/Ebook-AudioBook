d9dddf9193d3e0e8d6c79e4eed35f55658656761
SamDePaull <prima2100016041@webmail.uad.ac.id>
2024-07-24 03:17:16 +0700

    in

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const path = require('path');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -8,6 +7,18 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 const User = require('./user')(sequelize, Sequelize.DataTypes);
